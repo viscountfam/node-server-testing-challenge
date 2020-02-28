a467c2a84f3bf145089bfa223087dacc67f69c0a
viscountfam <V3ctor42!>
2020-02-28 08:08:12 -0500

    bugs fixed

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-
+const teamsRouter = require('../teams/router.js')
 
 
 const server = express();
--- teams/router.js
+++ teams/router.js
@@ -45,3 +45,5 @@ router.delete("/:id", (req, res) => {
   });
 
 })
+
+module.exports = router
